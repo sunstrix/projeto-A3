a12ff049090596f2bba1b3402513a66e16f4708a
sunstrix <apnogueira93@gmail.com>
2026-05-18 17:03:06 -0300

    Create UsuarioRepository interface for user management

---
+++ src/main/java/meuprojeto/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package meuprojeto.repository;
+
+import meuprojeto.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
