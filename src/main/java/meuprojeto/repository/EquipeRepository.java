a7c9510c6b255531aa4e0d741a1eae7b911de279
sunstrix <apnogueira93@gmail.com>
2026-05-18 17:03:57 -0300

    Create EquipeRepository interface for Equipe model

---
+++ src/main/java/meuprojeto/repository/EquipeRepository.java
@@ -0,0 +1,7 @@
+package meuprojeto.repository;
+
+import meuprojeto.model.Equipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipeRepository extends JpaRepository<Equipe, Long> {
+}
