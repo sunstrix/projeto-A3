f24214da9a6df5b3553b39dcdb79cbad38cb4a51
sunstrix <apnogueira93@gmail.com>
2026-05-21 16:03:14 -0300

    Delete src directory

--- src/main/java/meuprojeto/repository/EquipeRepository.java
+++
@@ -1,7 +0,0 @@
-package meuprojeto.repository;
-
-import meuprojeto.model.Equipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquipeRepository extends JpaRepository<Equipe, Long> {
-}
--- src/main/java/meuprojeto/repository/ProjetoRepository.java
+++
@@ -1,7 +0,0 @@
-package meuprojeto.repository;
-
-import meuprojeto.model.Projeto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjetoRepository extends JpaRepository<Projeto, Long> {
-}
